44a20042a543340720d58e88d97416f571f7e5a3
Vitor Barbosa <vitorqb@gmail.com>
2022-10-23 18:54:00 +0200

    Add black to the CI

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-USAGE="$0"' [-h]
+USAGE="$0"' [-h] [-c]
 
 Runs formatting for the project.
 
   -h)
     Displays this help message.
 
+  -c)
+    If given, only check the files and return an exit code.
 '
 
 # Getopts
-while getopts "h" opt; do
+while getopts "hc" opt; do
   case "$opt" in
     h)
         echo "$USAGE"
         exit 0
         ;;
     --)
-        shift
+        ;;
+    c)
+        CHECK=1
         ;;
     *)
         echo "ERROR: UNKNOWN ARGUMENT: $1" >&2
@@ -26,4 +30,9 @@ while getopts "h" opt; do
 done
 
 # Script
-poetry run black .
+ARGS=()
+if [ "$CHECK" = "1" ]
+then
+    ARGS+=( "--check" )
+fi
+poetry run black ${ARGS[@]} .
