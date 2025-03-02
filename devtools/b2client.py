08fad7a16c802ab0d3a55bee63587ae3ebf9f282
Vitor Barbosa <vitorqb@gmail.com>
2025-03-02 19:42:45 +0100

    fix(b2cleaner): fixes deletion of files
    Files were not being deleted from `b2` because we called `bucket.ls`
    in a way that we didn't see all files.
    
    Also add a new helper `dev` CLI to interact directly with the b2
    client for local testing.

---
+++ devtools/b2client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+A helper script that allows you to test whether the google drive client is working.
+"""
+import logging
+import typer
+from dailybkup import b2utils
+
+app = typer.Typer()
+
+@app.callback()
+def setup(
+    ctx: typer.Context,
+    application_key_id: str = typer.Option(..., envvar="DAILYBKUP_B2_APPLICATION_KEY_ID"),
+    application_key: str = typer.Option(..., envvar="DAILYBKUP_B2_APPLICATION_KEY"),
+    bucket_name: str = typer.Option(..., envvar="DAILYBKUP_B2_BUCKET_NAME"),
+    prefix: str = typer.Option("", envvar="DAILYBKUP_B2_PREFIX"),
+    log_level: str = typer.Option("DEBUG", envvar="LOG_LEVEL"),
+):
+    logging.basicConfig(level=log_level)
+    b2context = b2utils.B2Context(
+        application_key_id=application_key_id,
+        application_key=application_key,
+        bucket_name=bucket_name,
+        prefix=prefix,
+    )
+    ctx.obj = b2context
+
+@app.command()
+def delete_file(file_name: str, ctx: typer.Context):
+    """
+    Delete a file from the bucket based on it's name.
+    """
+    b2context = ctx.obj
+    b2context.delete(file_name)
+
+if __name__ == "__main__":
+    app()
